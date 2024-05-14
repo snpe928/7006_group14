1bca709b9ade2fbf73585dce5c56263385445a2a
snpe928 <166684316+snpe928@users.noreply.github.com>
2024-05-14 17:35:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 # Set page configuration
 st.set_page_config(
