ca553106be3324067aa6376d105d09bd50aa6490
snpe928 <166684316+snpe928@users.noreply.github.com>
2024-05-14 17:22:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
